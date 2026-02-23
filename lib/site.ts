8e06b810962330e658713ca54dda73c0afa6fdbd
Sharky121 <mail@netcats.pro>
2026-02-23 14:27:03 +0300

    video

---
+++ lib/site.ts
@@ -0,0 +1,10 @@
+export const SITE_NAME = "Усадьба Ушмор";
+
+export const SITE_URL =
+  process.env.NEXT_PUBLIC_SITE_URL ??
+  process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "https://usadba-na-pre.ru";
+
+export const SITE_DEFAULT_DESCRIPTION =
+  "Усадьба Ушмор — уникальное место для проведения свадьбы. Старинный парк, гостевые дома, изысканная природа Мещеры. Рязанская область.";
